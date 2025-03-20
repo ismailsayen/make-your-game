6f69f9780b05296e91a635c6ce81d83de9cfc57e
ismailsayen <ismailsvn02@gmail.com>
2025-03-20 22:55:39 +0000

    structred the files and th next tache is to mouve the paddle

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-const grid = document.querySelector(".grid");
-const blocks = document.querySelector(".blocks");
-const board=document.querySelector('.board')
-const blockWidth = grid.clientWidth / 7;
-let colors = ["#00eefb", "#f738fc"];
-let boardPostion=["40%",20]
-function DrawBlocks() {
-  for (let i = 0; i < 6 * 6; i++) {
-    let color = Math.floor(Math.random() * colors.length);
-    const block = document.createElement("div");
-    block.style.cssText = `
-      width: ${blockWidth}px;
-      height: 30px;
-      background-color: ${colors[color]};
-      box-sizing: border-box;
-      margin-bottom: 20px;
-      box-shadow: ${colors[color]} 0px 2px 8px 0px;
-      border-radius: 8px;
-      text-shadow: #fc0 1px 0 10px;
-    `;
-    blocks.appendChild(block);
-  }
-}
-DrawBlocks()
-board.style.bottom=`${boardPostion[1]}px`
-board.style.left=`${boardPostion[0]}`
+import { DrawBlocks } from "./js/drawBlocks.js";
+import { DrawBoard } from "./js/board.js";
+
+DrawBlocks();
+
+DrawBoard();
